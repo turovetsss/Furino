9712ef4eaf25c4e309b0a84b1bba08d62bc55167
vichklu <119040501+vichklu@users.noreply.github.com>
2023-09-24 21:42:06 +0300

    validation of forms

--- src/login/log.js
+++ src/login/log.js
@@ -4,20 +4,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const loginForm = document.getElementById("login-form");
   const loginButton = document.getElementById("login-form-submit");
   const loginErrorMsg = document.getElementById("login-error-msg");
-  
   loginButton.addEventListener("click", (e) => {
       e.preventDefault();
       const username = loginForm.username.value;
       const password = loginForm.password.value;
-  
-      if (username !== "" && password !== "") {
-        console.log("логин " + "username")
-        console.log("пароль " + "password")
-      } 
-      else {
-          loginErrorMsg.style.opacity = 1;
+
+       if (username !== "" && password !== "") {
+            const formData = {
+            username: username,
+            password: password,
+        };
+        console.log(formData);
       }
- 
+      else{
+    		loginErrorMsg.innerHTML = "Please fill the required fields"
+        loginErrorMsg.style.opacity = 1;
+      }
+
+    
   })
   
 });
