1375d84d630af9a5fa1dc2d81fd0cbcf87253e3f
vichklu <119040501+vichklu@users.noreply.github.com>
2023-10-03 21:56:50 +0300

    registration form validation 2 attempt

--- src/js/log.js
+++ src/js/log.js
@@ -1,28 +1,59 @@
+function printError(elemId, hintMsg) {
+  document.getElementById(elemId).innerHTML = hintMsg;
+}
 
-window.addEventListener("DOMContentLoaded", (event) => {
+function isValidUsername(username) {
+  if (username === "") {
+      return "Please fill in the fields";
+  } else {
+      var regex = /^[a-zA-Z\s]+$/;
+      if (regex.test(username) === false) {
+          return "Please, enter a valid name";
+      }
+  }
+  return "";
+}
 
+function isValidPassword(password) {
+  if (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_\-]).{8,}$/.test(password)) {
+      return "";
+  } else {
+      return "Password must contain at least 8 characters with an uppercase letter.";
+  }
+}
+
+function validateForm() {
   const loginForm = document.getElementById("login-form");
-  const loginButton = document.getElementById("login-form-submit");
-  const loginErrorMsg = document.getElementById("login-error-msg");
-  loginButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      const username = loginForm.username.value;
-      const password = loginForm.password.value;
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+  let formIsValid = true;
 
-       if (username !== "" && password !== "") {
-            const formData = {
-            username: username,
-            password: password,
-        };
-        console.log(formData);
-      }
-      else{
-    		loginErrorMsg.innerHTML = "Please fill the required fields"
-        loginErrorMsg.style.opacity = 1;
-      }
+  const usernameError = isValidUsername(username);
+  const passwordError = isValidPassword(password);
 
-    
-  })
-  
-});
+  if (usernameError) {
+      printError("nameError", usernameError);
+      formIsValid = false;
+  } else {
+      printError("nameError", "");
+  }
 
+  if (passwordError) {
+      printError("passwordError", passwordError);
+      formIsValid = false;
+  } else {
+      printError("passwordError", "");
+  }
+
+  if (formIsValid) {
+      const formData = {
+          username: username,
+          password: password
+      };
+      console.log(formData);
+
+      console.log("Form is valid. Sending data...");
+  }
+
+  return false; // предотвращаем обсерантус стандартного действия кнопки(не отправляем форму)
+}
--- src/login/log.js
+++ src/login/log.js
@@ -1,28 +1,49 @@
 
-window.addEventListener("DOMContentLoaded", (event) => {
-
-  const loginForm = document.getElementById("login-form");
-  const loginButton = document.getElementById("login-form-submit");
-  const loginErrorMsg = document.getElementById("login-error-msg");
-  loginButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      const username = loginForm.username.value;
-      const password = loginForm.password.value;
-
-       if (username !== "" && password !== "") {
-            const formData = {
+  function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+  } 
+function validForm(){
+    const loginForm = document.getElementById("login-form");
+    const loginButton = document.getElementById("login-form-submit");
+    let loginErr = document.getElementById("nameEror");
+    let passwordError=document.getElementById("passwordError");
+    var nameError=true;
+    let username = loginForm.username.value;
+    const password = loginForm.password.value;
+      function validLogin(username){
+        if(username=="")
+      {
+       printError("nameError","please fill in the fields")
+      }
+          else{
+        var regex=/^[a-zA-Z\s]+$/;
+        if(regex.test(username)===false){
+          printError("nameError","Please, enter a valid name");
+        }else{
+          printError("nameError","");
+          nameError=false;
+          const formData = {
             username: username,
             password: password,
         };
         console.log(formData);
+        }
       }
-      else{
-    		loginErrorMsg.innerHTML = "Please fill the required fields"
-        loginErrorMsg.style.opacity = 1;
       }
-
-    
-  })
+      
+        if(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password)){
+          console.log('zaebis');
+        }
+        else{
+          printError("passwordError","at least 8 characters containing a Upper case");
+          return false;
+        }
+      //  if (username !== "" && password !== "") {
+      //       const formData = {
+      //       username: username,
+      //       password: password,
+      //   };
+      //   console.log(formData);
+      // }
+  };
   
-});
-
